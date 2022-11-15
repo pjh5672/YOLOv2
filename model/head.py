fix: dont care anchor box index for avoiding loss computation

model/head.py
@@ -8,9 +8,8 @@ from element import Conv, weight_init_kaiming_uniform
 class YoloHead(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        feat_dims = 1024
-        self.conv = Conv(in_channels, feat_dims, kernel_size=3, padding=1)
-        self.detect = nn.Conv2d(feat_dims, out_channels, kernel_size=1)
+        self.conv = Conv(in_channels, 1024, kernel_size=3, padding=1)
+        self.detect = nn.Conv2d(1024, out_channels, kernel_size=1)
         self.apply(weight_init_kaiming_uniform)
 
 
@@ -31,7 +30,7 @@ if __name__ == "__main__":
     num_boxes = 5
     num_attributes = (1 + 4 + num_classes)
     device = torch.device('cpu')
-    backbone, feat_dims = build_backbone(arch_name='darknet19', pretrained=True)
+    backbone, feat_dims = build_backbone(pretrained=True)
     neck = PassthroughLayer(stride=2)
     head = YoloHead(in_channels=feat_dims[0] * 4 + feat_dims[1], out_channels=num_attributes * num_boxes)
 

model/yolo.py
@@ -1,8 +1,7 @@
 import sys
 from pathlib import Path
 
-FILE = Path(__file__).resolve()
-ROOT = FILE.parents[1]
+ROOT = Path(__file__).resolve().parents[1]
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
 
@@ -17,13 +16,13 @@ from utils import set_grid
 
 
 class YoloModel(nn.Module):
-    def __init__(self, input_size, backbone, num_classes, anchors):
+    def __init__(self, input_size, num_classes, anchors):
         super().__init__()
         self.num_boxes = 5
         self.input_size = input_size
         self.num_classes = num_classes
         self.num_attributes = 1 + 4 + num_classes
-        self.backbone, feat_dims = build_backbone(arch_name=backbone, pretrained=True)
+        self.backbone, feat_dims = build_backbone(pretrained=True)
         self.neck = PassthroughLayer(stride=2)
         self.head = YoloHead(in_channels=feat_dims[0]*4+feat_dims[1], out_channels=self.num_attributes*self.num_boxes)
         self.anchors = torch.tensor(anchors)
@@ -83,7 +82,7 @@ if __name__ == "__main__":
                [0.8605263,  0.8736842 ],
                [0.283375,   0.5775    ]]
 
-    model = YoloModel(input_size=input_size, backbone='darknet19', num_classes=num_classes, anchors=anchors).to(device)
+    model = YoloModel(input_size=input_size, num_classes=num_classes, anchors=anchors).to(device)
     model.train()
     out = model(inp.to(device))
     print(model.grid_size)

train.py
@@ -86,7 +86,6 @@ def parse_args(make_dirs=True):
     parser.add_argument("--resume", type=str, nargs='?', const=True ,help="Name to resume path")
     parser.add_argument('--multi_scale', action='store_true', help='Multi-scale training')
     parser.add_argument("--data", type=str, default="toy.yaml", help="Path to data.yaml")
-    parser.add_argument("--backbone", type=str, default="darknet19", help="Model architecture")
     parser.add_argument("--img_size", type=int, default=416, help="Model input size")
     parser.add_argument("--bs", type=int, default=64, help="Batch size")
     parser.add_argument("--nbs", type=int, default=64, help="Nominal batch size")
@@ -138,7 +137,7 @@ def main():
     args.train_size = args.img_size
     args.last_opt_step = -1
 
-    model = YoloModel(input_size=args.img_size, backbone=args.backbone, num_classes=len(args.class_list), anchors=args.anchors)
+    model = YoloModel(input_size=args.img_size, num_classes=len(args.class_list), anchors=args.anchors)
     macs, params = profile(deepcopy(model), inputs=(torch.randn(1, 3, args.img_size, args.img_size),), verbose=False)
     model = model.cuda(args.rank)
     criterion = YoloLoss(input_size=args.img_size, anchors=model.anchors)

utils/loss.py
@@ -18,8 +18,8 @@ class YoloLoss():
         self.lambda_noobj = 0.5
         self.lambda_coord = 5.0
         self.num_boxes = 5
-        self.iou_thres = 0.5
-        self.num_attributes = 1 + 4 + 1 + 1
+        self.iou_threshold = 0.5
+        self.num_attributes = 1 + 4 + 1
         self.obj_loss_func = nn.MSELoss(reduction='none')
         self.box_loss_func = nn.MSELoss(reduction='none')
         self.cls_loss_func = nn.CrossEntropyLoss(reduction='none')
@@ -31,7 +31,7 @@ class YoloLoss():
         self.device = predictions.device
         self.bs = predictions.shape[0]
         targets = self.build_batch_target(labels).to(self.device)
-        
+    
         with torch.no_grad():
             iou_pred_with_target = self.calculate_iou(pred_box_cxcywh=predictions[..., 1:5], target_box_cxcywh=targets[..., 1:5])
 
@@ -40,13 +40,13 @@ class YoloLoss():
         pred_box_twth = predictions[..., 3:5]
         pred_cls = predictions[..., 5:].permute(0, 3, 1, 2)
 
-        target_obj = targets[..., 0]
-        target_noobj = (targets[..., 6] == 0).float()
+        target_obj = (targets[..., 0] == 1).float()
+        target_noobj = (targets[..., 0] == 0).float()
         target_box_txty = targets[..., 1:3]
         target_box_twth = targets[..., 3:5]
         target_cls = targets[..., 5].long()
 
-        obj_loss = self.obj_loss_func(pred_obj, target_obj * iou_pred_with_target) * target_obj
+        obj_loss = self.obj_loss_func(pred_obj, iou_pred_with_target) * target_obj
         obj_loss = obj_loss.sum() / self.bs
         
         noobj_loss = self.obj_loss_func(pred_obj, pred_obj * 0) * target_noobj
@@ -93,17 +93,21 @@ class YoloLoss():
                 grid_j = (item[2] * self.grid_size).long()
                 ious_target_with_anchor = self.calculate_iou_target_with_anchors(target_wh=item[3:5], anchor_wh=self.anchors)
                 best_index = ious_target_with_anchor.max(dim=0).indices
-                keep = ious_target_with_anchor >= self.iou_thres
 
                 tx = (item[1] * self.grid_size) - grid_i
                 ty = (item[2] * self.grid_size) - grid_j
                 tw = torch.log(item[3] / self.anchors[best_index, 0])
                 th = torch.log(item[4] / self.anchors[best_index, 1])
 
-                target[grid_j, grid_i, best_index, 0] = 1.0
                 target[grid_j, grid_i, best_index, 1:5] = torch.tensor([tx, ty, tw, th])
                 target[grid_j, grid_i, best_index, 5] = cls_id
-                target[grid_j, grid_i, keep, 6] = 1.0
+                
+                for index, iou in enumerate(ious_target_with_anchor):
+                    if index == best_index:
+                        target[grid_j, grid_i, index, 0] = 1.0
+                    else:
+                        if iou >= self.iou_threshold:
+                            target[grid_j, grid_i, index, 0] = -1.0
             return target
 
     
@@ -161,7 +165,7 @@ if __name__ == "__main__":
     anchors = train_dataset.anchors
     num_classes = len(train_dataset.class_list)
     
-    model = YoloModel(input_size=input_size, backbone="darknet19", num_classes=num_classes, anchors=anchors).to(device)
+    model = YoloModel(input_size=input_size, num_classes=num_classes, anchors=anchors).to(device)
     criterion = YoloLoss(input_size=input_size, anchors=model.anchors)
     optimizer = optim.SGD(model.parameters(), lr=0.0001)
     optimizer.zero_grad()
@@ -175,11 +179,9 @@ if __name__ == "__main__":
             filenames, images, labels, ori_img_sizes = minibatch
             predictions = model(images.to(device))
             loss = criterion(predictions=predictions, labels=labels)
-            # loss.backward()
             loss[0].backward()
             optimizer.step()
             optimizer.zero_grad()
 
-            acc_loss += loss[0].item()
-            # acc_loss += loss[1].item() # multipart_loss, obj_loss, noobj_loss, txty_loss, twth_loss, cls_loss
+            acc_loss += loss[0].item() # multipart_loss, obj_loss, noobj_loss, txty_loss, twth_loss, cls_loss
         print(acc_loss / len(train_loader))