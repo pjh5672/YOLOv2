9dfab1fc6cc966af20df2299dababbbed1580f76
DESKTOP-OFEIAI7\pjh56 <pjh5672.dev@gmail.com>
2022-11-22 21:07:03 +0900

    chore

--- dataloader/__init__.py
+++ dataloader/__init__.py
@@ -1,11 +1,9 @@
 import sys
 from pathlib import Path
 
-FILE = Path(__file__).resolve()
-ROOT = FILE.parents[0]
+ROOT = Path(__file__).resolve().parents[0]
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
 
 from .dataset import Dataset
 from .transform import to_image, BasicTransform, AugmentTransform
-from .transform2 import BaseTransform, Augmentation
