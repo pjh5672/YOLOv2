b35307aa73d35f6e3236cf08447dd40f347fe0af
DESKTOP-OFEIAI7\pjh56 <pjh5672.dev@gmail.com>
2022-11-25 01:29:56 +0900

    chore

--- train.py
+++ train.py
@@ -133,6 +133,7 @@ def main():
     macs, params = profile(deepcopy(model), inputs=(torch.randn(1, 3, args.img_size, args.img_size),), verbose=False)
     model.set_grid_xy(input_size=args.train_size)
     model = model.cuda(args.rank)
+    criterion = YoloLoss(input_size=args.train_size, anchors=model.anchors)
     optimizer = optim.SGD(model.parameters(), lr=args.base_lr, momentum=args.momentum, weight_decay=args.weight_decay)
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=args.lr_decay, gamma=0.1)
     evaluator = Evaluator(annotation_file=args.mAP_file_path)
