d6fee875265bd9f27091f4b4aa03cae7a2679cfe
DESKTOP-OFEIAI7\pjh56 <pjh5672.dev@gmail.com>
2022-12-13 23:28:58 +0900

    change : act func in conv

--- model/element.py
+++ model/element.py
@@ -1,4 +1,14 @@
+import torch
 from torch import nn
+import torch.nn.functional as F
+
+
+class Mish(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x * torch.tanh(F.softplus(x))
 
 
 class Conv(nn.Module):
@@ -9,18 +19,18 @@ class Conv(nn.Module):
                 ### Depth-wise ###
                 nn.Conv2d(c1, c1, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=c1, bias=False),
                 nn.BatchNorm2d(c1),
-                nn.LeakyReLU(0.1, inplace=True) if act else nn.Identity(),
+                Mish() if act else nn.Identity(),
                 ### Point-wise ###
                 nn.Conv2d(c1, c2, kernel_size=1, stride=stride, padding=0, dilation=dilation, groups=1, bias=False),
                 nn.BatchNorm2d(c2),
-                nn.LeakyReLU(0.1, inplace=True) if act else nn.Identity(),
+                Mish() if act else nn.Identity(),
             )
         else:
             self.conv = nn.Sequential(
                 ### General Conv ###
                 nn.Conv2d(c1, c2, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=1, bias=False),
                 nn.BatchNorm2d(c2),
-                nn.LeakyReLU(0.1, inplace=True) if act else nn.Identity(),
+                Mish() if act else nn.Identity(),
             )
 
     def forward(self, x):
