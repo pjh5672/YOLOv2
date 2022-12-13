175f7129b09b79f9acd7ec7b8439ee1ec1a7d896
DESKTOP-OFEIAI7\pjh56 <pjh5672.dev@gmail.com>
2022-12-13 23:30:12 +0900

    chore

--- model/element.py
+++ model/element.py
@@ -3,12 +3,14 @@ from torch import nn
 import torch.nn.functional as F
 
 
+
 class Mish(nn.Module):
     def __init__(self):
         super().__init__()
 
     def forward(self, x):
         return x * torch.tanh(F.softplus(x))
+
 
 
 class Conv(nn.Module):
